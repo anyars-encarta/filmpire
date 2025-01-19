8e3752f37a57d867ead1ea3d11175c3fae306c74
Anyars Yussif <anyarsencarta@gmail.com>
2025-01-19 12:21:13 +0000

    Re-install react app

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+
+const App =() => {
+  return (
+    <div className="App">
+      <h1>Fimpire is ready!</h1>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
+import reportWebVitals from './reportWebVitals';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
+
+reportWebVitals();
