c39f00a6b1b89821621209cea71d91363fce6604
Anyars Yussif <anyarsencarta@gmail.com>
2025-01-21 11:40:39 +0000

    Implement Navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,81 @@
 import React from 'react';
+import {
+  AppBar, IconButton, Toolbar, Drawer, Button, Avatar, useMediaQuery,
+} from '@mui/material';
+import {
+  Menu, AccountCircle, Brightness4, Brightness7,
+
+} from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+import { useTheme } from '@mui/material/styles';
+import useStyles from './styles';
 
 const NavBar = () => {
-  console.log('Navbar');
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(max-width:600px)');
+  const theme = useTheme();
+  const isAuthenticated = true;
 
   return (
-    <div>NavBar</div>
+    <>
+      <AppBar position="fixed">
+        <Toolbar className={classes.toolbar}>
+          {isMobile && (
+            <IconButton
+              edge="start"
+              color="inherit"
+              style={{ outline: 'none' }}
+              onClick={() => {}}
+              className={classes.menuButton}
+            >
+              <Menu />
+            </IconButton>
+          )}
+          <IconButton
+            color="inherit"
+            sx={{ ml: 1 }}
+            onClick={() => {}}
+          >
+            {theme.palette.mode === 'dark' ? (
+              <Brightness7 />
+            ) : (
+              <Brightness4 />
+            )}
+          </IconButton>
+
+          {!isMobile && 'Search...'}
+
+          <div>
+            {!isAuthenticated ? (
+              <Button
+                color="inherit"
+                onClick={() => {}}
+              >
+                Login &nbsp;
+                <AccountCircle />
+              </Button>
+            ) : (
+              <Button
+                color="inherit"
+                component={Link}
+                to={`/profile/${1}`}
+                className={classes.linkButton}
+                onClick={() => {}}
+              >
+                {!isMobile && <>My Movies &nbsp;</>}
+                <Avatar 
+                  style={{ width: 30, height: 30 }}
+                  alt="Profile"
+                  src="https://avatars.githubusercontent.com/u/4723117?v=4"
+                />
+              </Button>
+            )}
+          </div>
+
+          {isMobile && 'Search...'}
+        </Toolbar>
+      </AppBar>
+    </>
   );
 };
 
