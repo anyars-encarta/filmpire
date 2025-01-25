d8e82213e4f758f215fc49125b8235601bd83249
Anyars Yussif <anyarsencarta@gmail.com>
2025-01-25 15:33:47 +0000

    Correct spacing errors

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { userSelector } from '../../features/auth';
 
-
 const Profile = () => {
   const { user } = useSelector(userSelector);
 
