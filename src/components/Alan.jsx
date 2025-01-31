b78fd03cb53e5e7c6ee72f7fdff4dd72c867f59b
Anyars Yussif <anyarsencarta@gmail.com>
2025-01-31 12:17:19 +0000

    Add Alan AI functionality to login and logout using voice prompts

--- src/components/Alan.jsx
+++ src/components/Alan.jsx
@@ -2,6 +2,7 @@ import { useEffect, useContext } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 
 import { ColorModeContext } from '../utils/ToggleColorMode';
+import { fetchToken } from '../utils';
 
 const useAlan = () => {
   const { setMode } = useContext(ColorModeContext);
@@ -17,6 +18,14 @@ const useAlan = () => {
           } else {
             setMode('dark');
           }
+        } else if (command === 'login') {
+          fetchToken();
+        } else if (command === 'logout') {
+          localStorage.removeItem('accountId');
+          localStorage.removeItem('request_token');
+          localStorage.removeItem('session_id');
+
+          window.location.href = '/';
         }
       },
     });
