c8712c563f83d76eb024044c645fbf4e1e98c1f5
Anyars Yussif <anyarsencarta@gmail.com>
2025-01-31 17:23:21 +0000

    Close sidebar on menu item select and display user image on the navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -100,7 +100,7 @@ const NavBar = () => {
                 <Avatar
                   style={{ width: 30, height: 30 }}
                   alt="Profile"
-                  src="https://avatars.githubusercontent.com/u/4723117?v=4"
+                  src={`https://www.themoviedb.org/t/p/w64_and_h64_face${user?.avatar?.tmdb?.avatar_path}`}
                 />
               </Button>
             )}
